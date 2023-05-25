ab7617edcba4e7178f61c7c21310134651108bf6
Shuo Ding <sdingcn@outlook.com>
2023-05-25 19:43:41 -0400

    lexer and parser

--- interpreter.py
+++ interpreter.py
@@ -7,7 +7,26 @@ def lex(source):
     chars = deque(source)
 
     def next_token():
-        pass
+        while chars and chars[0].isspace():
+            chars.popleft()
+        if chars:
+            if chars[0].isdigit():
+                token = ''
+                while chars and chars[0].isdigit():
+                    token += chars.popleft()
+            elif chars[0] in ('-', '+') and chars[1].isdigit():
+                token = chars.popleft()
+                while chars and chars[0].isdigit():
+                    token += chars.popleft()
+            elif chars[0].isalpha():
+                token = ''
+                while chars and chars[0].isalpha():
+                    token += chars.popleft()
+            elif chars[0] in ('(', ')', '{', '}', '[', ']', '+', '-', '*', '/', '%', '<'):
+                token = chars.popleft()
+            return token
+        else:
+            return None
 
     tokens = deque()
     while True:
@@ -29,9 +48,9 @@ class Var:
         self.name = name
 
 class Lambda:
-    def __init__(self, var_list, expr_list):
+    def __init__(self, var_list, expr):
         self.var_list = var_list
-        self.expr_list = expr_list
+        self.expr = expr
 
 class Letrec:
     def __init__(self, var_expr_list, expr):
@@ -66,40 +85,83 @@ def parse(tokens):
         return s.isalpha()
 
     def parse_int():
-        pass
+        value = int(tokens.popleft())
+        return Int(value)
 
     def parse_var():
-        pass
+        name = tokens.popleft()
+        return Var(name)
 
     def parse_lambda():
-        pass
+        tokens.popleft() # lambda
+        tokens.popleft() # (
+        var_list = []
+        while tokens[0].isalpha():
+            var_list.append(tokens.popleft())
+        tokens.popleft() # )
+        tokens.popleft() # {
+        expr = parse_expr()
+        tokens.popleft() # }
+        return Lambda(var_list, expr)
 
     def parse_letrec():
-        pass
+        tokens.popleft() # letrec
+        tokens.popleft() # (
+        var_expr_list = []
+        while tokens[0].isalpha():
+            v = tokens.popleft()
+            tokens.popleft() # =
+            e = parse_expr()
+            var_expr_list.append((v, e))
+        tokens.popleft() # )
+        tokens.popleft() # {
+        expr = parse_expr()
+        tokens.popleft() # }
+        return Letrec(var_expr_list, expr)
 
     def parse_if():
-        pass
+        tokens.popleft() # if
+        cond = parse_expr()
+        tokens.popleft() # then
+        branch1 = parse_expr()
+        tokens.popleft() # else
+        branch2 = parse_expr()
+        return If(cond, branch1, branch2)
 
     def parse_call():
-        pass
+        tokens.popleft() # (
+        fun = parse_expr()
+        arg_list = []
+        while tokens[0] != ')':
+            arg_list.append(parse_expr())
+        tokens.popleft() # )
+        return Call(fun, arg_list)
 
     def parse_seq():
-        pass
+        tokens.popleft() # [
+        expr_list = []
+        while tokens[0] != ']':
+            expr_list.append(parse_expr())
+        tokens.popleft() # ]
+        return Seq(expr_list)
 
-    if is_int(tokens[0]):
-        return parse_int()
-    elif is_var(tokens[0]):
-        return parse_var()
-    elif tokens[0] == 'lambda':
-        return parse_lambda()
-    elif tokens[0] == 'letrec':
-        return parse_letrec()
-    elif tokens[0] == 'if':
-        return parse_if()
-    elif tokens[0] == '(':
-        return parse_call()
-    elif tokens[0] == '[':
-        return parse_seq()
+    def parse_expr():
+        if is_int(tokens[0]):
+            return parse_int()
+        elif is_var(tokens[0]):
+            return parse_var()
+        elif tokens[0] == 'lambda':
+            return parse_lambda()
+        elif tokens[0] == 'letrec':
+            return parse_letrec()
+        elif tokens[0] == 'if':
+            return parse_if()
+        elif tokens[0] == '(':
+            return parse_call()
+        elif tokens[0] == '[':
+            return parse_seq()
+    
+    return parse_expr()
 
 # interpreting
 
@@ -118,6 +180,7 @@ class Frame:
 def interpret(tree):
     store = {}
     stack = []
+    env = []
 
 # main entry
 
