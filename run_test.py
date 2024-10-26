3bf74d4e4920ce38f6ed588846f80ff6ca2a9f32
Shuo Ding <sdingcn@outlook.com>
2024-10-25 22:11:21 -0400

    better test framework

--- run_test.py
+++ run_test.py
@@ -1,7 +1,9 @@
+import json
 import os
 import os.path
 import subprocess
 import sys
+import time
 from typing import List, Tuple, Union
 
 def execute(cmd: List[str], i: Union[None, str] = None) -> Tuple[int, str, str]:
@@ -22,17 +24,16 @@ if __name__ == "__main__":
                 filepath = os.path.join(dirpath, filename)
                 print(f"running test {filepath} ... ", end = "")
                 sys.stdout.flush()
-                inpath = filepath[:-3] + "in"
-                outpath = filepath[:-3] + "out"
-                errpath = filepath[:-3] + "err"
-                with open(inpath, "r") as f:
-                    res = execute(["build/closure", filepath], f.read())
-                with open(outpath, "r") as g:
-                    out = g.read()
-                with open(errpath, "r") as h:
-                    err = h.read()
-                if ((res[0] == 0) == (err == "") and res[1] == out and res[2] == err):
-                    print("passed")
+                iopath = filepath[:-3] + "io"
+                with open(iopath, "r") as f:
+                    io = json.loads(f.read())
+                start = time.time()
+                res = execute(["build/closure", filepath], io["in"])
+                end = time.time()
+                if ((res[0] == 0) == (io["err"] == "") and
+                    res[1] == io["out"] and
+                    res[2] == io["err"]):
+                    print(f"passed in {end - start:.3f} seconds")
                 else:
-                    sys.exit(f"failed\nres = {res}\ntruth = {(out, err)}")
+                    sys.exit(f'failed\nres = {res}\ntruth = {(io["out"], io["err"])}')
                 sys.stdout.flush()
---
+++ test/re.io
@@ -0,0 +1,5 @@
+{
+    "in" : "",
+    "out" : "3\n3\n",
+    "err" : "[runtime error (SourceLocation 5 11)] type error on intrinsic call\n"
+}
